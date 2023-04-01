f20f35921d62139bab910c8af51b87a296a2f85f
RIKHI SOBARI <RIKHI.SOBARI@btpn.com>
2023-04-02 02:38:47 +0700

    init

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ require('dotenv').config();
 
 const express = require('express');
 const app = express();
-const { Telegraf, Context } = require('telegraf');
-const { message } = require('telegraf/filters');
+const { Telegraf } = require('telegraf');
 const axios = require('axios');
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 8080;
 const { Configuration, OpenAIApi } = require('openai');
 
 const apiToken = process.env.TELEGRAM_TOKEN;
+const baseUrl = process.env.PATH;
 const url = 'https://api.telegram.org/bot';
 
 const bot = new Telegraf(apiToken);
@@ -17,43 +17,42 @@ const configuration = new Configuration({
 });
 const openai = new OpenAIApi(configuration);
 
-app.post('/', (req, res) => {
-  const chatId = req.body.message.chat.id;
-  const sentMessage = req.body.message.text;
-  console.log(req.body);
-  if (sentMessage.match(/hello/gi)) {
-    axios
-      .post(`${url}${apiToken}/sendMessage`, {
-        chat_id: chatId,
-        text: 'hello back 👋'
-      })
-      .then((response) => {
-        res.status(200).send(response);
-      })
-      .catch((error) => {
-        res.send(error);
-      });
-  } else {
-    res.status(200).send({});
+const init = async () => {
+  try {
+    const setWebhook = await axios.get(
+      `${url}${apiToken}/setWebhook?url=${baseUrl}`
+    );
+    console.log(setWebhook);
+  } catch (error) {
+    console.log(error);
   }
+};
 
-  // bot.hears(req.body.message.text, async (ctx) => {
-  //   try {
-  //     const completion = await openai.createChatCompletion({
-  //       model: 'gpt-3.5-turbo',
-  //       messages: [{ role: 'user', content: 'hello world' }]
-  //     });
-  //     const message = completion.data.choices[0].message.content;
-  //     bot.telegram.sendMessage(ctx.chat.id, message, {});
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // });
+app.post('/', async (req, res) => {
+  try {
+    console.log(req.body);
+  } catch (error) {
+    console.log(error);
+  }
 });
 
+// bot.hears('animal', async (ctx) => {
+//   try {
+//     const completion = await openai.createChatCompletion({
+//       model: 'gpt-3.5-turbo',
+//       messages: [{ role: 'user', content: 'hello world' }]
+//     });
+//     const message = completion.data.choices[0].message.content;
+//     bot.telegram.sendMessage(ctx.chat.id, message, {});
+//   } catch (error) {
+//     console.log(error);
+//   }
+// });
 // bot.launch();
-bot.telegram.setWebhook('https://telegram-bot-chatgpt-ghtg.onrender.com');
-app.listen(port, () => console.log('app is running'));
+app.listen(port, async () => {
+  console.log('app is running');
+  await init();
+});
 // bot
 //   .launch({
 //     webhook: {
